eaff584cd968cbaa4fc374d3fb56140b72c9b786
nhamilton1 <nickh3592@gmail.com>
2021-10-08 07:21:55 -0400

    starting

--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
 // build your server here and require it from index.js
+const express = require('express');
+const projectRouter = require('../api/project/router');
+const resourceRouter = require('../api/resource/router')
+const taskRouter = require('../api/task/router')
+const server = express();
+
+server.use(express.json());
+server.use('/api/project', projectRouter);
+server.use('/api/project', resourceRouter);
+server.use('/api/project', taskRouter);
+
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
